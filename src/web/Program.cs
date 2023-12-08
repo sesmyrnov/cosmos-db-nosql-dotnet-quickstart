c3e208b1a2860c96e3e00e93cd29143fa7a70c10
Sergiy Smyrnov <59627981+sesmyrnov@users.noreply.github.com>
2023-12-08 13:23:52 -0600

    Initial commit

---
+++ src/web/Program.cs
@@ -0,0 +1,53 @@
+using Azure.Identity;
+using Microsoft.Azure.Cosmos;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddSingleton<CosmosClient>((_) =>
+    {
+        // <create_client>
+        CosmosClient client = new(
+            accountEndpoint: builder.Configuration["AZURE_COSMOS_DB_NOSQL_ENDPOINT"]!,
+            tokenCredential: new DefaultAzureCredential()
+        );
+        // </create_client>
+        return client;
+    });
+}
+else
+{
+    builder.Services.AddSingleton<CosmosClient>((_) =>
+    {
+        // <create_client_client_id>
+        CosmosClient client = new(
+            accountEndpoint: builder.Configuration["AZURE_COSMOS_DB_NOSQL_ENDPOINT"]!,
+            tokenCredential: new DefaultAzureCredential(
+                new DefaultAzureCredentialOptions()
+                {
+                    ManagedIdentityClientId = builder.Configuration["AZURE_MANAGED_IDENTITY_CLIENT_ID"]!
+                }
+            )
+        );
+        // </create_client_client_id>
+        return client;
+    });
+}
+
+var app = builder.Build();
+
+app.UseDeveloperExceptionPage();
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
